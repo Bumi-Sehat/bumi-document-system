e56674845b1b3ddd54eb2f273ba852d427fd6194
Mark <mark.scrudder@gmail.com>
2021-08-23 12:35:13 -0700

    Added User Management stub

---
+++ src/pages/userManagement.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../components/navbar.jsx';
+
+const UserManagement = () => {
+  return (
+    <div>
+      <Navbar />
+      <div>
+        <br />
+        Lorem Ipsum
+      </div>
+    </div>
+  );
+};
+
+export default UserManagement;
