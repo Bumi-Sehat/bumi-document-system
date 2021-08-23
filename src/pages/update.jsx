f69979904685dc86db936025707ada9d42ef4536
Mark <mark.scrudder@gmail.com>
2021-08-23 12:34:06 -0700

    Added Update stub.

---
+++ src/pages/update.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../components/navbar.jsx';
+
+const Update = () => {
+  return (
+    <div>
+      <Navbar />
+      <div>
+        <br />
+        Lorem Ipsum
+      </div>
+    </div>
+  );
+};
+
+export default Update;
