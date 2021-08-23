16b590a3fdc7d6bfa469aab3feb86df6b5cc449c
Mark <mark.scrudder@gmail.com>
2021-08-23 12:29:28 -0700

    Added basic navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './navbar.css';
+
+const Navbar = () => {
+  return (
+    <div className='container'>
+      <Link className='navbar' to='/'>
+        <span className='home link'>
+          Home
+        </span>
+      </Link>
+      <Link className='navbar' to='/newdocument'>
+        <span className='new-document link'>
+          New Document
+        </span>
+      </Link>
+      <Link className='navbar' to='/update'>
+        <span className='update link'>
+          Update Document
+        </span>
+      </Link>
+      <Link className='navbar' to='/userManagement'>
+        <span className='user-management link'>
+          User Management
+        </span>
+      </Link>
+    </div>
+  )
+}
+
+export default Navbar;
