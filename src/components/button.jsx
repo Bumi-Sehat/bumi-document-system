b6346f913a901e142e865adc043698e11ff69af7
Carlj0666 <carlj1881@yahoo.com>
2021-09-02 16:06:23 -0700

    Added toggle to handleClick functionality

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -2,12 +2,21 @@ import React, {Component} from 'react'
 
 class Button extends Component {
 
-    
+    state = {
+        submit: false
+    }
+
+    handleClick = (e) => {
+        const clicked = this.state.submit
+        this.setState({
+            submit: !clicked
+        })
+    }
 
     render(){
         return(
             <>
-            <button onClick={this.handleClick}>Button</button>
+            <button onClick={this.handleClick}>{this.state.submit ? "Saved" : "Save?"}</button>
             </>
         )
 
