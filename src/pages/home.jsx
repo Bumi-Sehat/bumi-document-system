48245a06eef4c3335713ac8365332cba5ceb6343
Carlj0666 <carlj1881@yahoo.com>
2021-09-02 10:50:56 -0700

    updated file names per request

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,9 +3,9 @@ import Navbar from '../components/navbar.jsx';
 import Button from '../components/button.jsx';
 import Item from '../components/item.jsx';
 import List from '../components/list.jsx';
-import RadioButton from '../components/radio_button.jsx';
-import TextField from '../components/text_field.jsx';
-import DropDown from '../components/drop_down.jsx';
+import RadioButton from '../components/radioButton.jsx';
+import TextField from '../components/textField.jsx';
+import DropDown from '../components/dropDown.jsx';
 
 const Home = () => {
   return (
